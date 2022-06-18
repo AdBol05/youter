d16813e2f99c209a30b8627974feefb3d348d56f
AdBol05 <98588523+AdBol05@users.noreply.github.com>
2022-06-18 18:54:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ console.log(" / / /_/ / /_/ / /_/  __/ /    ");
 console.log("/_/\\____/\\__,_/\\__/\\___/_/     ");
 
 //resolve video IDs
-lines.forEach(line => {table.push(line.split("=")[1]);});
+lines.forEach(line => {
+        line = line.replace("&list","");
+        table.push(line.split("=")[1]);});
 ids = table.filter(element => {return element !== undefined;});
 
 //Configure YoutubeMp3Downloader with your settings
@@ -26,13 +28,15 @@ var YD = new YoutubeMp3Downloader({
     "allowWebm": false                      // Enable download from WebM sources (default: false)
 });
 
+console.log("\n");
+console.log("List of videos to be downloaded:");
 console.log(ids);
+console.log("progress: \n");
 
+//download all links and save mp3
 ids.forEach( id => {
-	//Download video and save as MP3 file
-	YD.download(id);
-	YD.on("finished", function(err, data) {console.log(JSON.stringify(data));});
-	YD.on("error", function(error) {console.log(error);});
-	YD.on("progress", function(progress) {console.log(JSON.stringify(progress));});
-//console.log(id);
+        YD.download(id);
+        YD.on("finished", function(err, data) {console.log('\x1b[32m%s\x1b[0m',JSON.stringify(data));console.log("\n");});
+        YD.on("error", function(error) {console.error('\x1b[31m%s\x1b[0m',error); console.log("\n");});
+        YD.on("progress", function(progress) {console.log(JSON.stringify(progress)); console.log("\n");});
 });
