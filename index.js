a0ce5caaaa83a8943a9464c5a4f76ff3dd09b988
AdBol05 <98588523+AdBol05@users.noreply.github.com>
2022-06-21 11:01:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var YD = new YoutubeMp3Downloader({
     "ffmpegPath": "ffmpeg",                 // FFmpeg binary location
     "outputPath": "./download",             // Output folder location
     "youtubeVideoQuality": "highestaudio",  // Desired video quality
-    "queueParallelism": ids.length,         // Download parallelism
+    "queueParallelism": 1,         // Download parallelism
     "progressTimeout": 500,                 // Interval in ms for the progress reports
     "allowWebm": false                      // Enable download from WebM sources
 });
