86a1f0cd59d19cca7c020886d468efd5094add21
AdBol05 <98588523+AdBol05@users.noreply.github.com>
2022-06-21 15:32:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ let lines = file.split("\n");
 let table = [];
 let ids = [];
 
+var outPath = "./download";
+if (!fs.existsSync(outPath)) {fs.mkdirSync(outPath);}
+
 console.log("__  __            __          ");
 console.log("\\ \\/ /___  __  __/ /____  _____");
 console.log(" \\  / __ \\/ / / / __/ _ \\/ ___/");
@@ -29,9 +32,9 @@ if (args[0] === "threads"){parallelism = args[1];}
 //YoutubeMp3Downloder setup
 var YD = new YoutubeMp3Downloader({
     "ffmpegPath": "ffmpeg",                 // FFmpeg binary location
-    "outputPath": "./download",             // output folder location
+    "outputPath": outPath,                  // output folder location
     "youtubeVideoQuality": "highestaudio",  // Desired video quality
-    "queueParallelism": parallelism,         // Download parallelism
+    "queueParallelism": parallelism,        // Download parallelism
     "progressTimeout": 500,                 // Interval in ms for the progress reports
     "allowWebm": false                      // Enable download from WebM sources
 });
@@ -42,10 +45,7 @@ console.log(ids);
 console.log("progress: \n");
 
 //download all links and save mp3
-ids.forEach( id => {
-        console.log("started process for id %s \n", id);
-        YD.download(id);
-});
+ids.forEach( id => {console.log("started process for id %s \n", id); YD.download(id);});
 
 YD.on("finished", function(err, data) {console.log('\x1b[32m%s\x1b[0m',"Downloaded MP3 to:", data.file); console.log("\n");});
 YD.on("error", function(error) {console.error('\x1b[31m%s\x1b[0m',error); console.log("\n");});
