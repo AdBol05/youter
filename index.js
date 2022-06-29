d44efec925c92b4a852ba6799f21bf754af08f62
AdBol05 <98588523+AdBol05@users.noreply.github.com>
2022-06-29 18:58:21 +0200

    cleaned and added comments

--- index.js
+++ index.js
@@ -14,28 +14,29 @@ console.log('\x1b[32m%s\x1b[0m'," \\  / __ \\/ / / / __/ _ \\/ ___/ ");
 console.log('\x1b[32m%s\x1b[0m'," / / /_/ / /_/ / /_/  __/ /        ");
 console.log('\x1b[32m%s\x1b[0m',"/_/\\____/\\__,_/\\__/\\___/_/     ");
 
-//check if URL file exists
+//check if URL file exists (create and exit if not)
 if (!fs.existsSync(config.URLpath)) {console.error('\x1b[31m%s\x1b[0m','\n Error: ' + config.URLpath + ' not found, created.\n PLease paste desired video links into this file.');
     fs.openSync(config.URLpath, 'w');
     process.exit(1);
 }
 
-const file = fs.readFileSync(config.URLpath).toString("utf-8");
-let lines = file.split("\n");
+const file = fs.readFileSync(config.URLpath).toString("utf-8"); //read URL file
+let lines = file.split("\n"); //get individual lines
+
 let table = []; //temporary ID storage for cleaning
 let ids = []; //list of video IDs
 let prn = 0; //number of processes finished
 let bars = {}; //loading bars object
 
-//check if output path exists
+//check if output path exists (create if not)
 if (!fs.existsSync(config.OutputPath)) {fs.mkdirSync(config.OutputPath);}
 
 //resolve video IDs
 lines.forEach(line => {
-    if (!line.includes('=')){line = line.split('').reverse().join('').replace("/", "=").split('').reverse().join('');}
-    line = line.replace("&list","");
-    table.push(line.split("=")[1]);});
-ids = table.filter(element => {return element !== undefined;}); //clean up
+    if (!line.includes('=')){line = line.split('').reverse().join('').replace("/", "=").split('').reverse().join('');} //if link doesn't contain "=", replace last "/" with "="
+    line = line.replace("&list",""); //get rid of "&list" for easier ID separation
+    table.push(line.split("=")[1]);}); //add separated IDs to table
+ids = table.filter(element => {return element !== undefined;}); //get rid of undefined elements
 
 if (ids[0] === undefined){console.error('\x1b[31m%s\x1b[0m',"\n Error: No video ID available"); process.exit(1);} //exit if no IDs available
 
@@ -44,6 +45,7 @@ if (args[0] === "threads"){ //set custom number of threads
     parallelism = args[1];
     if(isNaN(parallelism)){console.error('\x1b[31m%s\x1b[0m',"\n Error: Invalid argument"); process.exit(9);} //exit if input is not a number
 }
+
 //YoutubeMp3Downloder setup
 var YD = new YoutubeMp3Downloader({
     "ffmpegPath": config.ffmpegPath,        // FFmpeg binary location
@@ -67,20 +69,14 @@ console.log("progress: \n");
 //download all links and save mp3
 ids.forEach( id => {
     console.log("started process for id %s", id);
-    YD.download(id);
+    YD.download(id); //start download process
     bars[id] = new AsciiBar({ //set progress bar for every video
-        undoneSymbol: config.bar.undoneSymbol,
-        doneSymbol: config.bar.doneSymbol,
-        width: config.bar.length,
-        formatString: '#percent #bar',
-        total: 100,
-        enableSpinner: false,
-        lastUpdateForTiming: false,
-        autoStop : false,
-        print: true,
-        start: 0,
-        stream: process.stdout,
-        hideCursor: false,
+        undoneSymbol: config.bar.undoneSymbol,  //symbol for undone part of bar
+        doneSymbol: config.bar.doneSymbol,      //symbol for done part of bar
+        width: config.bar.length,               //width of bar
+        formatString: '#percent #bar',          //format of bar
+        autoStop : false,                       //disable autoStop
+        stream: process.stdout,                 //output stream
     });
 });
 
@@ -89,11 +85,11 @@ console.log("\n");
 YD.on("error", function(error) {console.error('\x1b[31m%s\x1b[0m',error); console.log("\n");}); //error reports
 YD.on("progress", function(progress) { //download progress reports
     bars[progress.videoId].update(progress.progress.percentage.toFixed()); //update progress bars
-    console.log('\x1b[32m%s\x1b[0m',progress.videoId);
+    console.log('\x1b[32m%s\x1b[0m',progress.videoId); //print video ID next to progress bar
 });
 YD.on("finished", function(err, data) { //download finished
     console.log('\x1b[32m%s\x1b[0m',"\n Downloaded MP3 to:", data.file); //print download path
     console.log("\n");
-    prn++;
+    prn++; //ncrease counter of finished downloads
     if (prn >= ids.length){process.exit(0);} //exit when all downloads finished
 });
