02743c36777e435dffbff0f697b805d48b27d95f
AdBol05 <adam.b@volny.cz>
2025-10-26 01:42:08 +0200

    TODO ffmpeg transcode

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const AsciiBar = require('ascii-bar').default;
-const fs = require("fs");
-var YoutubeMp3Downloader = require("youtube-mp3-downloader");
+//import AsciiBar from 'ascii-bar';
+import * as fs from 'fs';
+//const { createFFmpeg } = require('@ffmpeg/ffmpeg');
+import { YtDlp } from 'ytdlp-nodejs';
+const ytdlp = new YtDlp();
 
 if(fs.existsSync("./config.json")) {var config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));} //read config if possible
 else{console.error('\x1b[31m%s\x1b[0m',"\n Error: Failed to read config.json. File does not exist."); process.exit(1);} //exit if config file is not found
@@ -48,15 +50,7 @@ if (args[0] === "threads"){ //set custom number of threads
     if(isNaN(parallelism)){console.error('\x1b[31m%s\x1b[0m',"\n Error: Invalid argument"); process.exit(9);} //exit if input is not a number
 }
 
-//YoutubeMp3Downloder setup
-var YD = new YoutubeMp3Downloader({
-    "ffmpegPath": config.ffmpegPath,        // FFmpeg binary location
-    "outputPath": config.OutputPath,        // output folder location
-    "youtubeVideoQuality": config.Quality,  // Desired video quality
-    "queueParallelism": parallelism,        // Download parallelism
-    "progressTimeout": config.ReportDelay,  // Interval in ms for the progress reports
-    "allowWebm": config.AllowWebM           // Enable download from WebM sources
-});
+
 
 //basic info
 console.log("\n");
@@ -73,23 +67,58 @@ console.log("Clear URL file: " + config.ClearURL);
 console.log("--------------------------------");
 console.log("progress: \n");
 
-//download all links and save mp3
 ids.forEach( id => {
-    console.log("started process for id %s", id);
-    YD.download(id); //start download process
-    bars[id] = new AsciiBar({ //set progress bar for every video
-        undoneSymbol: config.bar.undoneSymbol,  //symbol for undone part of bar
-        doneSymbol: config.bar.doneSymbol,      //symbol for done part of bar
-        width: config.bar.length,               //width of bar
-        formatString: '#percent #bar',          //format of bar
-        autoStop : false,                       //disable autoStop
-        stream: process.stdout,                 //output stream
+    /*bars[id] = new AsciiBar({
+        undoneSymbol: config.bar.undoneSymbol,
+        doneSymbol: config.bar.doneSymbol,
+        width: config.bar.length,
+        formatString: '#percent #bar',
+        autoStop : false,
+        stream: process.stdout,
+    });*/
+    let url = `https://www.youtube.com/watch?v=${id}`;
+    ytdlp.getTitleAsync(url).then((title) => {
+        console.log(`started process for id ${id} -> ${title}`);
+        ytdlp.downloadAsync(
+            url,
+            {
+                format: {
+                    filter: "audioonly",
+                    type: "mp3",
+                    quality: config.Quality
+                },
+                //filename: title,
+                output: `${config.OutputPath}/${title.trim()}.mp3`,
+                postProcessors: [{
+                    key: "FFmpegExtractAudio",
+                    preferredCodec: "mp3",
+                    preferredQuality: "192",
+                }],
+                onProgress: (progress) => {
+                    if(progress.status === 'downloading'){
+                        console.log(`\x1b[32m[${id}]\x1b[0m ${title.trim()}\n\x1b[32m[${progress.percentage_str}]\x1b[0m ${progress.downloaded_str}/${progress.total_str} @ ${progress.speed_str} - ${progress.eta_str}\n`);
+                    }
+                    else if(progress.status === 'finished'){
+                        console.log('\x1b[32m%s\x1b[0m',"\n Downloaded MP3 to:", `${config.OutputPath}/${title.trim()}.mp3`, ` ${progress.total_str}`);
+                        prn++;
+                        if (prn >= ids.length){
+                            if(config.ClearURL){console.log("Clearing URL file"); fs.writeFile(config.URLpath, "", function(error){console.error(error);});}
+                            process.exit(0);
+                        }
+                    }
+                    else {
+                        console.log("Unknown status!");
+                        console.log(progress);
+                    }
+                } 
+            }
+        );
     });
 });
 
 console.log("\n");
 
-YD.on("error", function(error) {console.error('\x1b[31m%s\x1b[0m',error); console.log("\n");}); //error reports
+/*YD.on("error", function(error) {console.error('\x1b[31m%s\x1b[0m',error); console.log("\n");}); //error reports
 YD.on("progress", function(progress) { //download progress reports
     bars[progress.videoId].update(progress.progress.percentage.toFixed()); //update progress bars
     console.log('\x1b[32m%s\x1b[0m',progress.videoId); //print video ID next to progress bar
@@ -105,4 +134,4 @@ YD.on("finished", function(err, data) { //download finished
         }
         process.exit(0); //exit when all downloads finished
     }
-});
+});*/
